428871aedf12b12ee20e3b7bd6391fccdd9a71f4
andreydnb <a.gritsay2@gmail.com>
2021-09-06 23:39:38 +0300

    delete

--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-const refs = {
-    range: document.getElementById('font-size-control'),
-    currentText: document.getElementById('text')
-}
-refs.range.addEventListener('input', onInputChange)
-
-function onInputChange() {
-    refs.currentText.style.fontSize = `${refs.range.value}px`;
-}
