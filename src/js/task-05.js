fda123a160ffc32aedc8df5a38fe5e2817a68adc
andreydnb <a.gritsay2@gmail.com>
2021-09-06 23:28:05 +0300

    update task 04-07

---
+++ src/js/task-05.js
@@ -0,0 +1,16 @@
+const refs = {
+input: document.querySelector('#name-input'),
+span: document.querySelector('#name-output'),
+}
+
+refs.input.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+    if (event.currentTarget.value === '') {
+    refs.span.textContent = 'незнакомец';
+}
+    else {
+    refs.span.textContent = event.currentTarget.value;    
+   }   
+}
+
